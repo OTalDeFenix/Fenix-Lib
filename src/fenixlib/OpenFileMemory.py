d62be492de0ce6bf4307cd1aa5c15577eb6b8888
OTalDeFenix <arthurhcastanha@gmail.com>
2024-02-22 19:07:45 -0300

    Update OpenFileMemory.py

--- src/fenixlib/OpenFileMemory.py
+++ src/fenixlib/OpenFileMemory.py
@@ -6,14 +6,13 @@ import os
 def OpenFileMemory():
 	config = configparser.ConfigParser()
 	if os.path.exists("config.ini"):
+		config.read('config.ini')
+		path = config.getboolean('EXCEL', 'Local_da_tabela')
+		return path
+	else:
 		config.add_section('CONFIGS')
 		config.add_section('EXCEL')
 		config.set('EXCEL', 'Local_da_tabela', filedialog.askopenfilename())
 		with open('config.ini', 'w') as config_file:
 			config.write(config_file)
-	else:
-		config.read('config.ini')
-		path = config.getboolean('EXCEL', 'Local_da_tabela')
-		return path
-
-print(path)
+		
