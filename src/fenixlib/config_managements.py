3a7a25203074524ad4eaa343443907217a7990a1
OTalDeFenix <arthurhcastanha@gmail.com>
2024-02-22 20:05:18 -0300

    adjusted the code to get the right type o vars

--- src/fenixlib/config_managements.py
+++ src/fenixlib/config_managements.py
@@ -7,7 +7,7 @@ def OpenFileMemory():
 	config = configparser.ConfigParser()
 	if os.path.exists("config.ini"):
 		config.read('config.ini')
-		path = config.getboolean('EXCEL', 'Local_da_tabela')
+		path = config.get('EXCEL', 'Local_da_tabela')
 		return path
 	else:
 		config.add_section('CONFIGS')
@@ -15,4 +15,6 @@ def OpenFileMemory():
 		config.set('EXCEL', 'Local_da_tabela', filedialog.askopenfilename())
 		with open('config.ini', 'w') as config_file:
 			config.write(config_file)
-		
+		config.read('config.ini')
+		path = config.get('EXCEL', 'Local_da_tabela')
+		return path
