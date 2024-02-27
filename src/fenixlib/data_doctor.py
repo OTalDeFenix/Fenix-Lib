5259f784796c0dfffd692647aca304c65d9579f8
OTalDeFenix <arthurhcastanha@gmail.com>
2024-02-27 20:45:32 -0300

    Removed QuickSwap function temporarily for fixing and added write for debug function that outputs the value of a variable to a file

--- src/fenixlib/data_doctor.py
+++ src/fenixlib/data_doctor.py
@@ -19,12 +19,20 @@ def OpenFileMemory():
 		path = config.get('EXCEL', 'Local_da_tabela')
 		return path
 
-def QuickSwap(Urls, Swaps):
-    Replaces = []
-    for item in Urls:
-        replaced_item = item  # Inicializa replaced_item com item original
-        for Key_url, Name in Swaps:
-            if Key_url in replaced_item:
-                replaced_item = replaced_item.replace(Key_url, Name)
-        Replaces.append(replaced_item)  # Adiciona o item substituído à lista Replaces
-    return Replaces
+def WriteForDebug(VaribleToDebug, FileNameForDebug):
+	FileDebug = FileNameForDebug()
+	config = configparser.ConfigParser()
+	config.add_section('DEBUG')
+	config.set('DEBUG', VaribleToDebug, filedialog.askopenfilename())
+	with open(FileDebug, 'w') as DebugFile:
+		config.write(DebugFile)
+    
+#def QuickSwap(Urls, Swaps):
+#    Replaces = []
+#    for item in Urls:
+#        replaced_item = item  # Inicializa replaced_item com item original
+#        for Key_url, Name in Swaps:
+#            if Key_url in replaced_item:
+#                replaced_item = replaced_item.replace(Key_url, Name)
+#        Replaces.append(replaced_item)  # Adiciona o item substituído à lista Replaces
+#    return Replaces
